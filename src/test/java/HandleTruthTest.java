96ae3c0f85110fce38343f5e1e5b91ea15f67e9f
Dani <curious.ideas1@gmail.com>
2020-11-21 19:08:44 -0800

    Minor improvements

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class HandleTruthTest {
 
-    // Test Keys
+    // Verify key output is correct
     @Test
     public void test1() {
         String msg = "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to";
@@ -24,12 +24,11 @@ public class HandleTruthTest {
         assertFalse(HandleTruth.wordCount(msg).containsKey(-3));
     }
 
-    // Test Size
+    // Verify size is correct
     @Test
     public void test2() {
         String msg = "hi hi mango coconut car hi hi";
 
-        // Verify size is correct
         assertTrue(HandleTruth.wordCount(msg).size() == 2);
         assertFalse(HandleTruth.wordCount(msg).size() == -1);
         assertFalse(HandleTruth.wordCount(msg).size() == 2.1);
@@ -41,7 +40,7 @@ public class HandleTruthTest {
         assertTrue(HandleTruth.wordCount(msg3).size() == 0);
     }
 
-    // Test Values
+    // Verify value output is correct
     @Test
     public void test3() {
         String msg = ". hot egg quail man man good band hot , . sauce ( ( ( ( ) ) )";
@@ -56,7 +55,7 @@ public class HandleTruthTest {
         assertEquals("[[4], [1]]", HandleTruth.wordCount(msg).values().toString());
     }
 
-    // BUG - Line 9 in HandleTruth fixes this bug
+    // CATCHES BUG - Line 9 in HandleTruth fixes this bug
     @Test
     public void test5() {
         String msg = "";
@@ -99,7 +98,7 @@ public class HandleTruthTest {
         assertEquals("[[a, b, c]]", HandleTruth.wordCount(msg).values().toString());
     }
 
-    // BUG - Line 7 in HandleTruth fixes this bug
+    // CATCHES BUG - Line 7 in HandleTruth fixes this bug
     @Test
     public void test11() {
         String msg = " a";
